699fd3c6766b6bf6005df142fee23405720799cd
Pranav2612000 <pranav2000joglekar@gmail.com>
2020-12-02 11:47:49 +0530

    feat: render a snake object inside the background square

--- src/main.rs
+++ src/main.rs
@@ -11,19 +11,47 @@ use opengl_graphics::{GlGraphics, OpenGL};
 
 struct Game {
     gl: GlGraphics,
+    snake: Snake,
 }
 
 impl Game {
     fn render(&mut self, arg: &RenderArgs) {
         use graphics;
         
-        let BG_COLOR: [f32; 4] = [1.0, 1.0, 1.0, 1.0];
+        let BG_COLOR: [f32; 4] = [1.0, 1.0, 1.0, 1.0]; // white
 
         self.gl.draw(arg.viewport(), | _c, gl| {
             graphics::clear(BG_COLOR, gl);
         });
+
+        self.snake.render(&mut self.gl, arg);
     }
 }
+
+struct Snake {
+    pos_x: i32,
+    pos_y: i32,
+}
+
+impl Snake {
+    fn render(&self, gl: &mut GlGraphics, args: &RenderArgs) {
+        use graphics;
+        
+        let SNAKE_COLOR: [f32; 4] = [0.0, 0.0, 0.0, 1.0]; //black
+
+        let square  = graphics::rectangle::square(self.pos_x as f64,
+                                                  self.pos_y as f64, 
+                                                  20_f64);
+
+        gl.draw(args.viewport(), |c, gl| {
+            let transform = c.transform;
+
+            graphics::rectangle(SNAKE_COLOR, square, transform, gl);
+        });
+        
+    }
+}
+    
 
 fn main() {
     let opengl = OpenGL::V3_2;
@@ -37,7 +65,8 @@ fn main() {
         .unwrap();
 
     let mut game = Game {
-        gl: GlGraphics::new(opengl)
+        gl: GlGraphics::new(opengl),
+        snake: Snake { pos_x: 50, pos_y: 50 },
     };
 
     /* Add event loop */
